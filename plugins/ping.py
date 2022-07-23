c1b99600cefff3f98a295982800b81c7e2b9a32b
sanith2005 <77660641+sanith2005@users.noreply.github.com>
2022-07-23 18:36:27 +0530

    Create ping.py

---
+++ plugins/ping.py
@@ -0,0 +1,12 @@
+import time
+from pyrogram import Client, filters
+from pyrogram.types.messages_and_media.message import Message
+
+@Client.on_message(filters.command("ping"))
+def ping(_, message:Message):
+    if not AuthUserCheck(message): return
+    if ForceSub(message) == 400: return
+    start_time = int(round(time.time() * 1000))
+    reply = message.reply_text("Ping")
+    end_time = int(round(time.time() * 1000))
+    reply.edit_text(f"Pong\n{end_time - start_time} ms")
