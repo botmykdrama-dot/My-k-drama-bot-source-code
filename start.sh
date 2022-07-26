4b9113486897e8ca2f1c550f48fc86eea2631418
Sanith Bimsara <77660641+sanithbimsara@users.noreply.github.com>
2022-07-26 10:05:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /My-k-drama-bot-source-code
 fi
-cd /EvaMaria
+cd /My-k-drama-bot-source-code
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
