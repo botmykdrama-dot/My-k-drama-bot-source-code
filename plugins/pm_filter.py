77883b8f2362029ce0c35c1e230b1bb33b37f2f8
Sanith Bimsara <77660641+sanithbimsara@users.noreply.github.com>
2022-07-26 10:24:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -442,7 +442,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('〄 Updates', url='https://t.me/myKdrama_botupdats'),
             InlineKeyboardButton('📊 Status', callback_data='stats')
         ], [
-            InlineKeyboardButton('Rate ⭐', url='https://t.me/tlgrmcbot?start=mykdrama_bot-review'),                                 
+            InlineKeyboardButton('Rate ⭐', url='https://t.me/tlgrmcbot?start=mykdrama_bot-review'),   
+            InlineKeyboardButton('View on GitHub 🎁', url='https://github.com/sanithbimsara/My-K-Drama-bot'),              
         ], [
             InlineKeyboardButton('🏡 Home', callback_data='start'),
             InlineKeyboardButton('✖️ Close', callback_data='close_data')
